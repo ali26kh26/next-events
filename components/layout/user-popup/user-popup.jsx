dc30fe73e6f5736ff7a84755565999c6aab02581
ali26kh26 <fcb.akh.26@gmail.com>
2022-09-08 16:37:41 +0430

    create user popup

---
+++ components/layout/user-popup/user-popup.jsx
@@ -0,0 +1,40 @@
+import classes from "./user-popup.module.scss";
+import { AiOutlineUser, AiOutlineHeart, AiOutlineLogout } from "react-icons/ai";
+import { signOut } from "next-auth/react";
+import Link from "next/link";
+const UserPopUp = () => {
+  return (
+    <ul className={classes.container}>
+      <li>
+        <Link href="/profile">
+          <div className={classes.action}>
+            <div className={classes.icon}>
+              <AiOutlineUser />
+            </div>
+            <p> profile </p>
+          </div>
+        </Link>
+      </li>
+      <li>
+        <Link href="/events">
+          <div className={classes.action}>
+            <div className={classes.icon}>
+              <AiOutlineHeart />
+            </div>
+            <p> Favorites </p>
+          </div>
+        </Link>
+      </li>
+      <li onClick={() => signOut()}>
+        <div className={classes.action}>
+          <div className={classes.icon}>
+            <AiOutlineLogout />
+          </div>
+          <p> Logout </p>
+        </div>
+      </li>
+    </ul>
+  );
+};
+
+export default UserPopUp;
